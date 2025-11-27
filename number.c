34e8830b2e8e564b5f429523b9302fd150789d7e
divyasreekumkala-18 <divyasreekumkala@gmail.com>
2025-11-27 14:41:19 +0530

    largest among 3 numbers

---
+++ number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the values of a,b,c:");
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>b && a>c)
+    {
+        printf(" %d is larger ", a);
+    }
+    else if(b>c && b>a)
+    {
+        printf(" %d is larger", b);
+    }
+    else 
+    {
+        printf(" %d is larger", c);    }
+}
