e9e29467a7bc54f0807f8b75646e26022ad4d96a
divyasreekumkala-18 <divyasreekumkala@gmail.com>
2025-11-27 12:21:56 +0530

    arithmetic operators

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a=20,b=4,res;
+    res = a + b;
+    printf("a + b is %d\n",res);
+    res = a - b;
+    printf("a - b is %d\n",res);
+    res = a * b;
+    printf("a * b is %d\n",res);
+    res = a / b;
+    printf("a / b is %d\n",res);
+    res = a % b;
+    printf("a %% b is %d\n",res);
+    return 0;
+}
